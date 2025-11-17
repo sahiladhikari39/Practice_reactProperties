4f0deecf0a4b3bacf55e28cad8f036a666a2a4f3
Sahil Adhikari <sahiladhikari39@gmail.com>
2025-11-17 22:18:20 +0545

    learned to map data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts"
 
+function createCard(contact){
+  return (<Card key={contact.id} name={contact.name} img={contact.imgURL} num={contact.phone} email={contact.email} />);
+}
+
+
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card name={contacts[0].name} img={contacts[0].imgURL} num={contacts[0].phone} email={contacts[0].email} />
+      {contacts.map(createCard)}
+      {/* <Card name={contacts[0].name} img={contacts[0].imgURL} num={contacts[0].phone} email={contacts[0].email} />
       <Card name={contacts[1].name} img={contacts[1].imgURL} num={contacts[1].phone} email={contacts[1].email} />
-      <Card name={contacts[2].name} img={contacts[2].imgURL} num={contacts[2].phone} email={contacts[2].email} />
+      <Card name={contacts[2].name} img={contacts[2].imgURL} num={contacts[2].phone} email={contacts[2].email} /> */}
 
     </div>
   );
